9b6fb2b045056f320652b80ca8226673aa1d7c29
cclia19 <97136786+cclia19@users.noreply.github.com>
2026-03-17 15:59:03 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Lunar Director' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
